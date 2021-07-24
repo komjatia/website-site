a1b6e5fd4e49be4972a0c87fa72a841dcb75d951
komjatia <komjati.akos156@icloud.com>
2021-07-24 16:58:48 +0200

    changes

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+export default function Header() {
+  return (
+    <header>
+      <Navbar bg='primary' variant='dark' collapseOnSelect expand='lg'>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>business website</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='navbarScroll' />
+          <Navbar.Collapse id='navbarScroll'>
+            <Nav
+              className='my-2 my-lg-0 justify-content-end align-items-center'
+              style={{ maxHeight: "100px", width: "100%" }}
+              navbarScroll>
+              <LinkContainer to='/'>
+                <Nav.Link>Fooldal</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/about'>
+                <Nav.Link>Rolunk</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/services'>
+                <Nav.Link>Szolgaltatasok</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/pricing'>
+                <Nav.Link>Araink</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/contact'>
+                <Nav.Link>
+                  <button type='button' className='btn btn-success'>
+                    Kapcsolat
+                  </button>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
